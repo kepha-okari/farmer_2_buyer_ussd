80ab6394668f1b3cbbf30e4b2ee37c98a4d94ca7
rkepha <kephaokari@gmail.com>
2018-04-14 12:54:01 +0300

    added a custom User class

--- farm_to_pocket/models.py
+++ farm_to_pocket/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    type_of_user = models.CharField(max_length=2, null=True)
+    phonenumber = models.CharField(max_length=20, null=True)
+    level = models.IntegerField(null=True)
+    location = models.CharField(max_length=50)
+    nearest_town = models.CharField(max_length=50)
+
+    @classmethod
+    def requested_users(cls, user_type):
+        '''
+        Method that gets list of farmers for buyer and vice versa
+        '''
+        if user_type == '1':
+            requested_type = '2'
+        else:
+            requested_type = '1'
+
+        found_users = User.objects.filter(type_of_user=requested_type)
+        return found_users
+# Create your models here.
 class Buyer(models.Model):
     name = models.CharField(max_length=30, null=True)
     phonenumber = models.CharField(max_length=20, null=True)
